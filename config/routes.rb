9da059f9fc1a5a407f77ae242cea278d80c3c3d1
OlukaDenis <olukadeno@gmail.com>
2020-03-06 02:54:45 +0300

    Added the categories model and associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   root to: 'articles#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_210530) do
+ActiveRecord::Schema.define(version: 2020_03_05_232707) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "author_id"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2020_03_05_210530) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["author_id"], name: "index_articles_on_author_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "priority"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
